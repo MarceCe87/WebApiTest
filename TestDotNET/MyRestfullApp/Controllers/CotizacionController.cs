44bad64fcbb6c77824f35d09ff43db873aa396ec
MarceCe87 <marcelocereseto@gmail.com>
2018-09-15 19:46:20 -0300

    .

---
+++ TestDotNET/MyRestfullApp/Controllers/CotizacionController.cs
@@ -0,0 +1,35 @@
+﻿using MyRestfullApp.EstrategiasMoneda;
+using System;
+using System.Web.Http;
+
+namespace MyRestfullApp.Controllers
+{
+    public class CotizacionController : ApiController
+    {
+        [Route("MyRestfullApp/Cotizacion/{moneda}")]
+        [HttpGet()]
+        public IHttpActionResult GetCotizacion(string moneda)
+        {
+            CotizacionContext cn = new CotizacionContext();
+
+            if (moneda.Equals("Dolar", StringComparison.InvariantCultureIgnoreCase))
+            {
+                cn.SetCotizador(new CotizacionDolarService());
+            }
+            else if (moneda.Equals("Peso", StringComparison.InvariantCultureIgnoreCase))
+            {
+                cn.SetCotizador(new CotizacionPesoService());
+            }
+            else if (moneda.Equals("Real", StringComparison.InvariantCultureIgnoreCase))
+            {
+                cn.SetCotizador(new CotizacionRealService());
+            }
+            else
+            {
+                cn.SetCotizador(new CotizacionOtraService());
+            }
+
+            return cn.Cotizar();
+        }
+    }
+}
---
+++ TestDotNET/MyRestfullApp/EstrategiasMoneda/CotizacionPesoService.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+
+namespace MyRestfullApp.EstrategiasMoneda
+{
+    public class CotizacionPesoService : ApiController, ITipoCotizacion
+    {
+        public IHttpActionResult Cotizar()
+        {
+            this.Configuration = new HttpConfiguration();
+            this.Request = new HttpRequestMessage();
+            return Unauthorized();
+        }
+    }
+}
